062ccc977e80eb3381e076c63d386670372c2e55
wwyuj <jack123_it@163.com>
2020-08-27 01:11:19 +0800

    添加hello方法

--- src/com/huawei/Main.java
+++ src/com/huawei/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("hello world!");
+        System.out.println("hello world!!");
     }
 
 
